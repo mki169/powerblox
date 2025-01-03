e3f5b833a98712736f9d0945b8cdb4a98d66fc2b
mki169 <162124491+mki169@users.noreply.github.com>
2025-01-03 20:54:51 +0800

    Update 4483381587.lua

--- 4483381587/4483381587.lua
+++ 4483381587/4483381587.lua
@@ -2,7 +2,6 @@
 print("loader works")
 local counter = 1
 while counter <= 100 do
-  print("hi", "counter:", counter)
-  counter = counter += 1
+    print("hi", "counter:", counter)
+    counter += 1  -- Use counter = counter + 1 instead of counter += 1
 end
-  
